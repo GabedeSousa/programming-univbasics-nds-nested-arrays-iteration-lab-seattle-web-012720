3c264e29006035852991e5cbb0267663d2da4015
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-03 23:59:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,7 +9,7 @@ def join_ingredients(src)
   new_array = []
   i = 0 
   while i <  src.length do 
-    new array.push("I love #{src[i] [0]} and #{src[i] [1]}")
+    new array.push("I love #{src[i] [0]} and #{src[i] [1]} on my pizza")
 end
 
 def find_greater_pair(src)
