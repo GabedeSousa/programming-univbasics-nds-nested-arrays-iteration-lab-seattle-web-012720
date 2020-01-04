3ec0c3b73ba16d24d6d9a5b4e89d190f272321b5
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-04 00:13:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -43,7 +43,7 @@ def total_even_pairs(src)
   i = 0 
   while i < src.length do 
     if (src[i] [0] % 2 == 0) && (src[i] [1] % 2 == 0)
-      total +=  
+      total += (src[i] [0] +) 
     end
     total 
 end
