936775557cb1528b97917f0480c38c9ff634343f
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-04 00:04:32 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -21,6 +21,11 @@ def find_greater_pair(src)
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   
+  new_array = []
+  i = 0 
+  while i < src.length do
+    new_array.push
+  
 end
 
 def total_even_pairs(src)
